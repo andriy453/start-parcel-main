262196fef3788eecdf2ff182598249f74c24e214
andriy453 <125152793+andriy453@users.noreply.github.com>
2024-05-12 18:25:09 +0000

    Deploying to gh-pages from @ andriy453/start-parcel-main@3f2fae2dbc05d30a0952d4c392652bc5c9bf5a68 🚀

---
+++ assets/scripts/main.js
@@ -0,0 +1,7 @@
+// import './utils/vh.utils.js';
+// import './utils/animate-on-view.utils';
+
+// import './components/uploadFile.component.js';
+// import './components/inputs.component.js';
+
+// import './libs/countUp.lib.js';
